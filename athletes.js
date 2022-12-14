4c6ce793d129bb79d3c48aaca97b6c762bb774e2
Affapple <affectingapple1@gmail.com>
2022-12-14 19:46:13 +0000

    atletas js/html

--- athletes.js
+++ athletes.js
@@ -1,7 +1,11 @@
-function athletesTableViewModel() {
+let vm = function athletesTableViewModel() {
     let self = this;
 
-    self.name = ko.observable("Name")
+    self.baseUri = ko.observable('http://192.168.160.58/Olympics/api/athletes/FullDetails?id=');
+    self.name = ko.observable("Name");
+    self.sex = ko.observable("Sex");
+    self.Height = ko.observable("Height");
+    self.Weight = ko.observable("Weight");
     self.records = ko.observableArray([]);
     self.currentPage = ko.observable(1);
     self.pagesize = ko.observable(20);
@@ -16,5 +20,59 @@ function athletesTableViewModel() {
     self.nextPage = ko.computed(function(){
         return parseInt(self.currentPage()) + 1 ;
     }, self);
+
+
     
-}
+    function showLoading() {
+        $("#loadingModal").modal('show', {
+            backdrop: 'static',
+            keyboard: false
+        });
+    };
+    function hideLoading() {
+        $('#loadingModal').on('shown.bs.modal', function (e) {
+            $("#loadingModal").modal('hide');
+        })
+    };
+
+    function pedidoAJAX(uri, method){
+        self.error("idk tf im doing tbh")
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: "json",
+            data: JSON.stringify(data),
+            
+            beforeSend: function(){console.log(method, " ", uri, "...")},
+            
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("AJAX Call[" + uri + "] Fail...");
+                self.error(errorThrown);
+            
+            },
+            success: function (){
+                console.log("AJAX CAll", method, uri,"\nSuccess")
+            }
+        })
+    };
+
+    function startLoading(){
+        showLoading();
+        id = 0
+        do {
+            data = pedidoAJAX(self.baseUri + (id++))
+        } while(data.length > 0);
+        hideLoading();
+    };
+
+    // inicializar pedido
+    startLoading()
+};
+
+
+
+
+$(document).ready(function(){
+    ko.applyBindigs(new vm);
+
+});
